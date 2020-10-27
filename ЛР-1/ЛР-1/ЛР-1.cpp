7bd23f2087c3536130cf7dbf1994c44cee51314e
Alex <aleksim.dev@gmail.com>
2020-10-27 18:41:04 +0200

    add first and second labs

---
+++ ЛР-1/ЛР-1/ЛР-1.cpp
@@ -0,0 +1,138 @@
+﻿#include <iostream>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+#include <cmath>
+
+
+using namespace std;
+
+//ф-ция проверки целого числа
+bool isInt(float num) {
+	if (num == round(num)) return true;
+	else return false;
+}
+
+int main()
+{
+	int V = (24 % 5) ? 24 % 5 : 5;
+	cout << "Variant #" << V << endl;
+
+	int taskN;
+
+	cout << "Enter a number of task: ";
+	cin >> taskN;
+
+	switch (taskN)
+	{
+	case 1:
+	{
+		cout << "### TASK #1 ###" << endl;
+		int firstNumber, secondNumber;
+		float result;
+		cout << "Enter a first number: ";
+		cin >> firstNumber;
+		cout << "Enter a second number: ";
+		cin >> secondNumber;
+		result = (2 * cos(firstNumber) + sqrt(3) * sin(4 * secondNumber) - 1) / (2 * sin(secondNumber * secondNumber) + cos(42) - exp(pow(3, 1 / 3)));
+		cout << "Result: " << result << endl;
+		break;
+	}
+	case 2:
+	{
+		cout << "### TASK #2 ###" << endl;
+		int height, radius;
+		float volume, square;
+		cout << "Enter a radius: ";
+		cin >> radius;
+		cout << "Enter a height: ";
+		cin >> height;
+		volume = M_PI * radius * radius * height;
+		square = 2 * M_PI * radius * (radius + height);
+		cout << "V =  " << volume << endl;
+		cout << "S =  " << square << endl;
+		break;
+	}
+	case 3:
+	{
+		cout << "### TASK #3 ###" << endl;
+		int k, p, t;
+		cout << "Enter the integer numbers k, p and t in accordance: ";
+		cin >> k >> p >> t;
+		int tempP = p;
+		p = t;
+		t = k;
+		k = tempP;
+		cout << "k become a " << k << endl;
+		cout << "p become a " << p << endl;
+		cout << "t become a " << t << endl;
+		break;
+	}
+	case 4:
+	{
+		cout << "### TASK #4 ###" << endl;
+		int num, c1, c2, c3;
+
+		cout << "Enter a three-digit number: ";
+		cin >> num;
+		c1 = num / 100;        // сотни
+		c2 = (num % 100) / 10; //десятки
+		c3 = (num % 100) % 10; //единицы
+		cout << "Result: " << c2 << c1 << c3 << endl;
+		break;
+	}
+	case 5:
+	{
+		// решил решать следующим образом: во сколько полных пакетов одного типа влезет груз
+		cout << "### TASK #5 ###" << endl;
+
+		int userCargo;
+
+		struct PackageType
+		{
+			int package;
+			int box;
+			int container;
+		};
+
+		struct PackageCount
+		{
+			int package;
+			int box;
+			int container;
+		};
+
+		cout << "Enter a cargo: ";
+		cin >> userCargo;
+		int cargo = userCargo; //чтобы не менять исходное значение веса груза, иммутабельные данные
+
+		PackageType package = { 12.0, 60.0, 300.0 };
+		PackageCount packageCount = { 0, 0, 0 };
+
+		float packages = cargo / package.package;
+		float boxes = cargo / package.box;
+		float containers = cargo / package.container;
+
+		// вариант 1
+		packageCount.package = !isInt(packages) ? packages : ceil(packages) + 1;
+		packageCount.box = !isInt(boxes) ? boxes : ceil(boxes) + 1;
+		packageCount.container = !isInt(containers) ? containers : ceil(containers) + 1;
+
+		cout << "We need " << packageCount.package << " packages or  "
+			<< packageCount.box << " boxes or " << packageCount.container << " containers to load " << userCargo << "kg of cargo" << endl;
+		//конец первого варианта
+		// вариант 2 
+
+
+		break;
+	}
+	default: {
+		cout << "Task " << taskN << " isn't found" << endl;
+		break;
+	}
+	}
+
+	return 0;
+}
+
+
---
+++ ЛР-2/ЛР-2/ЛР-2.cpp
@@ -0,0 +1,211 @@
+﻿//#include <iostream>
+//#include <cmath>
+//
+//using namespace std;
+//
+////function for 4th task
+//int tringleParams(float length) {
+//	cout << "a = " << length << "; b = " << length << "; c = " << length * sqrt(2) << "; h = " << length / sqrt(2) << "; S = " << (length * length) / 2 << endl;
+//	return 0;
+//}
+//
+//int main()
+//{
+//	setlocale(LC_ALL, "ukrainian");
+//	int V = (21 % 5) ? 21 % 5 : 5;
+//	cout << "Варiант №" << V << endl;
+//
+//	int taskN;
+//
+//	cout << "Введiть номер завдвння: ";
+//	cin >> taskN;
+//
+//	switch (taskN)
+//	{
+//	case 1: {
+//		cout << "### Завдання #1 ###" << endl;
+//		int firstNumber, secondNumber;
+//		float result;
+//		cout << "Введiть перше число: ";
+//		cin >> firstNumber;
+//		cout << "Введiть друге число: ";
+//		cin >> secondNumber;
+//		if (firstNumber <= 0 && secondNumber > 0) {
+//			result = exp(firstNumber * secondNumber * secondNumber);
+//		}
+//		else if (firstNumber > 0 && secondNumber > 0 && firstNumber != 1) { // одз логирифма
+//
+//			result = log(secondNumber) / log(firstNumber); // first number - основа логарифма
+//		}
+//		else {
+//			cout << "Неможливо визначити, перевiрте правильнiсть вводу!" << endl;
+//			break; // конечный ответ, завершение кейса
+//		}
+//		cout << "Результат: " << result << endl;
+//
+//		break;
+//	}
+//	case 2: {
+//		cout << "### Завдання #2 ###" << endl;
+//		int x, y;
+//		cout << "Введiть x: ";
+//		cin >> x;
+//		cout << "Введiть y: ";
+//		cin >> y;
+//		if (x * x + y * y >= 4 && x * x + y * y <= 25) {
+//			cout << "Точка з коорданатами " << x << " та " << y << " iснує на графiку" << endl;
+//		}
+//		else {
+//			cout << "Точка з коорданатами " << x << " та " << y << " не iснує на графiку" << endl;
+//		}
+//		break;
+//	}
+//		  // Переделать, начиная с этого задания! Сделал не свой вариант!
+//	case 3: {
+//		cout << "### Завдання #3 ###" << endl;
+//		int A, B, C, temp;
+//		cout << "Введiть A: ";
+//		cin >> A;
+//		cout << "Введiть B: ";
+//		cin >> B;
+//		cout << "Введiть C: ";
+//		cin >> C;
+//
+//		if (A > B && A > C) {
+//			//A the biggest
+//			if (C > B) {
+//				C = B;
+//			}
+//			cout << "A = " << A << "; C = " << C << endl;
+//
+//		}
+//		else if (B > A && B > C) {
+//			//B the biggest
+//			if (C > A) {
+//				C = A;
+//			}
+//			A = B;
+//			cout << "A = " << A << "; C = " << C << endl;
+//		}
+//		else {
+//			//C the biggest
+//			temp = C;
+//			if (A > B) {
+//				C = B;
+//			}
+//			else  C = A;
+//			A = temp;
+//			cout << "A = " << A << "; C = " << C << endl;
+//		}
+//
+//		break;
+//	}
+//	case 4: {
+//		cout << "### Завдання #4 ###" << endl;
+//		int param;
+//		float length;
+//		cout << "Введiть значення: ";
+//		cin >> length;
+//		cout << "Введiть значення(1 – катет a, 2 – гiпотенуза с, 3 - висота h, опущена на гiпотенузу, 4 - площа S): ";
+//		cin >> param;
+//
+//		switch (param) {
+//		case 1: {
+//			tringleParams(length);
+//			break; }
+//		case 2: {
+//			length /= sqrt(2);
+//			tringleParams(length);
+//			break; }
+//		case 3: {
+//			length *= sqrt(2);
+//			tringleParams(length);
+//			break; }
+//		case 4: {
+//			length = sqrt(2 * length);
+//			tringleParams(length);
+//			break; }
+//		default: {
+//			cout << "Такого значення не знайдено" << endl;
+//			break;
+//		}
+//		}
+//
+//		break;
+//	}
+//	case 5: {
+//		cout << "### Завдання #5 ###" << endl;
+//		int mark;
+//		cout << "Введiть оцiнку: ";
+//		cin >> mark;
+//		switch (mark)
+//		{
+//		case 1:
+//		case 2:
+//		case 3:
+//		case 4:
+//		case 5:
+//		{
+//			cout << "Незадовiльно" << endl;
+//			break;
+//		}
+//		case 6:
+//		case 7:
+//		case 8:
+//		{
+//			cout << "Задовiльно" << endl;
+//			break;
+//		}
+//		case 9:
+//		case 10:
+//		{
+//			cout << "Добре" << endl;
+//			break;
+//		}
+//		case 11:
+//		case 12:
+//		{
+//			cout << "Вiдмiнно" << endl;
+//			break;
+//		}
+//		default: {
+//			cout << "Невiрне значення оцiнки!" << endl;
+//			break;
+//		}
+//		}
+//
+//		break;
+//	}
+//
+//	default: {
+//		cout << "Завдання " << taskN << " не знайдено" << endl;
+//		break;
+//	}
+//	}
+//	return 0;
+//}
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+
+int main()
+{
+	int userN, x1, y1, x2, y2;
+	cout << "Enter 1, 2 or 3: " << endl;
+	cin >> userN;
+	switch (userN)
+	{
+	case 1: {break; }
+	case 1: {break; }
+	case 1: {break; }
+
+	default: {
+		cout << "Wrong data \n"
+			break; }
+	}
+	return 0;
+}
